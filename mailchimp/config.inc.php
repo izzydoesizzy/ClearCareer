c59f6397531659c5cdeb079c98cbcd485d774c31
Iskender Piyale-Sheard <ipiyale@gmail.com>
2016-01-14 22:18:48 +0800

    MailChimp Update

--- mailchimp/config.inc.php
+++ mailchimp/config.inc.php
@@ -5,10 +5,10 @@ $apikey			= '7be52571dfc80a21d55be2a67e3f428a-us6'; // YOUR MAILCHIMP APIKEY
     
 // A List Id to run examples against. use lists() to view all
 // Also, login to MC account, go to List, then List Tools, and look for the List ID entry
-$listId			= 'adf0e21abb'; // YOUR MAILCHIMP LIST ID - see lists() method
+$listId			= '53b3044faf'; // YOUR MAILCHIMP LIST ID - see lists() method
     
 // A Campaign Id to run examples against. use campaigns() to view all
-$campaignId		= ''; // YOUR MAILCHIMP CAMPAIGN ID - see campaigns() method
+$campaignId		= 'HomepageSubscribe'; // YOUR MAILCHIMP CAMPAIGN ID - see campaigns() method
 
 // just used in xml-rpc examples
 $apiUrl			= 'http://api.mailchimp.com/1.3/';
